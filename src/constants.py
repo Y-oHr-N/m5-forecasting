f7899bb3cbadcd94b15137f3daebe6560143acf0
Y-oHr-N <kon.y.ohr.n@gmail.com>
2020-04-02 17:41:19 +0900

    Use arithmetical features

--- src/constants.py
+++ src/constants.py
@@ -26,12 +26,15 @@ calendar_features = [
     "is_month_end",
 ]
 lag_features = [f"{target}_shift_{i}" for i in [28]]
+arithmerical_features = [
+    "sell_price_day_over_day",
+]
 aggregated_features = [f"{target}_shift_1_rolling_{i}_mean" for i in [28]]
 numerical_features = [
     "snap_CA",
     "snap_TX",
     "snap_WI",
     "sell_price",
-] + calendar_features + lag_features + aggregated_features
+] + calendar_features + lag_features + arithmerical_features + aggregated_features
 
 features = categorical_features + numerical_features
