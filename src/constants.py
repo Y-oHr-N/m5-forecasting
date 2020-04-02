4f29e3b08cbf55913e1356038232a20594c78445
Y-oHr-N <kon.y.ohr.n@gmail.com>
2020-04-02 11:37:15 +0900

    Use aggregated featuures

--- src/constants.py
+++ src/constants.py
@@ -25,12 +25,13 @@ calendar_features = [
     "is_month_start",
     "is_month_end",
 ]
-lag_features = [f"{target}_shift_{i}" for i in range(28, 29)]
+lag_features = [f"{target}_shift_{i}" for i in [28]]
+aggregated_features = [f"{target}_shift_1_rolling_{i}_mean" for i in [28]]
 numerical_features = [
     "snap_CA",
     "snap_TX",
     "snap_WI",
     "sell_price",
-] + calendar_features + lag_features
+] + calendar_features + lag_features + aggregated_features
 
 features = categorical_features + numerical_features
