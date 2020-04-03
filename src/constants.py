e72ecc317f790f51863bc91789618eb07c85fe00
Y-oHr-N <kon.y.ohr.n@gmail.com>
2020-04-03 08:06:17 +0000

    Use demand_shift_22_rolling_7_mean

--- src/constants.py
+++ src/constants.py
@@ -25,16 +25,36 @@ calendar_features = [
     "is_month_start",
     "is_month_end",
 ]
-lag_features = [f"{target}_shift_{i}" for i in [28]]
+lag_features = [
+    f"{target}_shift_{i}" for i in [
+        28,
+    ]
+] + [
+    # f"sell_price_shift_{i}" for i in [
+    #     1,
+    #     22,
+    #     28,
+    # ]
+]
 arithmerical_features = [
     "sell_price_day_over_day",
+    # "sell_price_month_over_month",
 ]
-aggregated_features = [f"{target}_shift_1_rolling_{i}_mean" for i in [28]]
+aggregated_features = [
+    f"{target}_shift_{i}_rolling_{j}_mean" for (i, j) in [
+        (1, 28),
+        (22, 7),
+        # (22, 28),
+    ]
+]
 numerical_features = [
     "snap_CA",
     "snap_TX",
     "snap_WI",
     "sell_price",
+    # "is_california_holiday",
+    # "is_texas_holiday",
+    # "is_wisconsin_holiday",
 ] + calendar_features + lag_features + arithmerical_features + aggregated_features
 
 features = categorical_features + numerical_features
