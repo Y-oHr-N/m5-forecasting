96608a2e8cbd8bfbcb6524b95507109093cd5238
Y-oHr-N <kon.y.ohr.n@gmail.com>
2020-04-07 15:28:44 +0000

    Fix one ahead prediction

--- src/package/constants.py
+++ src/package/constants.py
@@ -1,4 +1,27 @@
-target = "demand"
+periods = [7, 28]
+windows = [7, 28]
+
+calendar_features = [
+    "year",
+    # "dayofyear",
+    "weekofyear",
+    "quarter",
+    "month",
+    "day",
+    "weekday",
+    # "is_year_start",
+    # "is_year_end",
+    # "is_quarter_start",
+    # "is_quarter_end",
+    # "is_month_start",
+    # "is_month_end",
+]
+lag_features = [f"demand_shift_{i}" for i in periods]
+aggregated_features = [
+    f"demand_shift_{i}_rolling_{j}_mean"
+    for i in periods
+    for j in windows
+]
 
 categorical_features = [
     "store_id",
@@ -12,51 +35,15 @@ categorical_features = [
     "event_type_2",
 ]
 
-calendar_features = [
-    "year",
-    "dayofyear",
-    "weekofyear",
-    "quarter",
-    "month",
-    "day",
-    "weekday",
-    "is_year_start",
-    "is_year_end",
-    "is_month_start",
-    "is_month_end",
-]
-lag_features = [f"{target}_shift_{i}" for i in [28,]] + [
-    # f"sell_price_shift_{i}" for i in [
-    #     1,
-    #     22,
-    #     28,
-    # ]
-]
-arithmerical_features = [
-    "sell_price_day_over_day",
-    # "sell_price_month_over_month",
-]
-aggregated_features = [
-    f"{target}_shift_{i}_rolling_{j}_mean"
-    for (i, j) in [
-        (1, 28),
-        (22, 7),
-        # (22, 28),
-    ]
-]
 numerical_features = (
     [
         "snap_CA",
         "snap_TX",
         "snap_WI",
         "sell_price",
-        # "is_california_holiday",
-        # "is_texas_holiday",
-        # "is_wisconsin_holiday",
     ]
     + calendar_features
     + lag_features
-    + arithmerical_features
     + aggregated_features
 )
 
--- src/package/feature_extraction.py
+++ src/package/feature_extraction.py
@@ -1,79 +1,21 @@
-import numpy as np
-import pandas as pd
-from workalendar.usa import California
-from workalendar.usa import Texas
-from workalendar.usa import Wisconsin
+from .constants import *
 
-from .constants import calendar_features
-from .utils import reduce_memory_usage
-
-
-def create_no_demand_period(is_zero):
-    out = np.empty_like(is_zero, dtype="int32")
-    state = 0
-
-    for i, elm in enumerate(is_zero):
-        if elm:
-            state += 1
-        else:
-            state = 0
-
-        out[i] = state
-
-    return out
+__all__ = ["extract_features"]
 
 
 def extract_features(df):
-    cals = [
-        California(),
-        Texas(),
-        Wisconsin(),
-    ]
-
-    intermediate = df["date"].unique()
-    intermediate = pd.DataFrame(intermediate, columns=["date"])
-
-    for cal in cals:
-        intermediate[f"is_{cal.__class__.__name__.lower()}_holiday"] = intermediate[
-            "date"
-        ].apply(cal.is_holiday)
-
-    df = df.merge(intermediate, how="left", on="date")
-
-    # Create missing indicators
-    df["sell_price_isnull"] = df["sell_price"].isnull()
-
     # Create calendar features
     for col in calendar_features:
         df[col] = getattr(df["date"].dt, col)
 
     grouped = df.groupby(["store_id", "item_id"])
 
-    # Create lag features
-    for i in [1, 22, 28]:
+    for i in periods:
+        # Create lag features
         df[f"demand_shift_{i}"] = grouped["demand"].shift(i)
-        df[f"sell_price_shift_{i}"] = grouped["sell_price"].shift(i)
 
-    # Create arithmetical features
-    df["sell_price_day_over_day"] = df["sell_price"] / df["sell_price_shift_1"]
-    df["sell_price_month_over_month"] = df["sell_price"] / df["sell_price_shift_28"]
-
-    # Create aggregated features
-    for i in [1, 22]:
-        for j in [7, 28]:
+        # Create aggregated features
+        for j in windows:
             df[f"demand_shift_{i}_rolling_{j}_mean"] = grouped[
                 f"demand_shift_{i}"
-            ].transform(lambda x: x.rolling(j, min_periods=1).mean())
-
-    df["sell_price_notnull_and_demand_shift_28_is_zero"] = (
-        ~df["sell_price_isnull"]
-    ) & (df["demand_shift_28"] == 0)
-    df["no_demand_period_shift_28"] = grouped[
-        "sell_price_notnull_and_demand_shift_28_is_zero"
-    ].transform(create_no_demand_period)
-
-    df.drop(columns="sell_price_notnull_and_demand_shift_28_is_zero", inplace=True)
-
-    reduce_memory_usage(df)
-
-    return df
+            ].transform(lambda x: x.rolling(j).mean())
