2262728d9f81e44a39e1a1e553631ef2faa08fc1
Y-oHr-N <kon.y.ohr.n@gmail.com>
2020-05-09 00:03:38 +0000

    Rename extract_features to create_lag_features

--- src/package/feature_extraction.py
+++ src/package/feature_extraction.py
@@ -1,16 +1,14 @@
 from .constants import *
 
-__all__ = ["extract_features"]
+__all__ = ["create_lag_features"]
 
 
-def extract_features(df):
+def create_lag_features(df):
     grouped = df.groupby(["store_id", "item_id"])
 
     for i in periods:
-        # Create lag features
         df[f"demand_shift_{i}"] = grouped["demand"].shift(i)
 
-        # Create aggregated features
         for j in windows:
             df[f"demand_shift_{i}_rolling_{j}_mean"] = grouped[
                 f"demand_shift_{i}"
