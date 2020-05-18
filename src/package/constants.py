167d5a2e1f8030ec172c93cc6de66edf638f8c93
Y-oHr-N <kon.y.ohr.n@gmail.com>
2020-05-19 00:09:47 +0900

    Add read_kwargs to constants

--- src/package/constants.py
+++ src/package/constants.py
@@ -1,3 +1,25 @@
+dtype = {
+    "wm_yr_wk": "int16",
+    "year": "int16",
+    "month": "int8",
+    "wday": "int8",
+    "event_name_1": "category",
+    "event_name_2": "category",
+    "event_type_1": "category",
+    "event_type_2": "category",
+    "snap_CA": "bool",
+    "snap_TX": "bool",
+    "snap_WI": "bool",
+    "state_id": "category",
+    "store_id": "category",
+    "cat_id": "category",
+    "dept_id": "category",
+    "item_id": "category",
+    "sell_price": "float32",
+}
+
+parse_dates = ["date"]
+
 train_start_date = "2011-01-29"
 train_end_date = "2016-03-28"
 validation_start_date = "2016-04-24"
