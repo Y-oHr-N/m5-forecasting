4bfa05a57d3f5d0a0d48a72eec080dd2644a212e
Y-oHr-N <kon.y.ohr.n@gmail.com>
2020-05-23 20:59:40 +0900

    Fix constants

--- src/package/constants.py
+++ src/package/constants.py
@@ -33,31 +33,11 @@ target = "sales"
 agg_funcs = ["min", "max", "mean", "std", "nunique"]
 agg_funcs_for_rolling = ["mean", "std"]
 
-periods = [7, 28]
+periods_batch = [28]
+periods_online = [7]
+periods = periods_online + periods_batch
 windows = [7, 28]
-max_lags = max(periods) + max(windows) - 1
-
-aggregated_features = [f"sell_price_{agg_func}" for agg_func in agg_funcs]
-
-calendar_features = [
-    "year",
-    "dayofyear",
-    "weekofyear",
-    "month",
-    "quarter",
-    "day",
-    "weekofmonth",
-    "weekday",
-]
-
-lag_features = [f"{target}_shift_{i}" for i in periods] + [
-    f"{target}_shift_{i}_rolling_{j}_{agg_func}"
-    for i in periods
-    for j in windows
-    for agg_func in agg_funcs_for_rolling
-]
-
-pct_change_features = [f"sell_price_pct_change_{i}" for i in periods]
+max_lags = max(periods_online) + max(windows) - 1
 
 binary_features = [
     "snap",
@@ -74,12 +54,39 @@ categorical_features = [
     "event_type",
 ]
 
+aggregated_features = [f"sell_price_{agg_func}" for agg_func in agg_funcs]
+
+calendar_features = [
+    "year",
+    "dayofyear",
+    "weekofyear",
+    "month",
+    "quarter",
+    "day",
+    "weekofmonth",
+    "weekday",
+]
+
+pct_change_features = [f"sell_price_pct_change_{i}" for i in periods]
+
+rolling_features = [
+    f"{target}_shift_{i}_rolling_{j}_{agg_func}"
+    for i in periods
+    for j in windows
+    for agg_func in agg_funcs_for_rolling
+]
+
+shift_features_batch = [f"{target}_shift_{i}" for i in periods_batch]
+shift_features_online = [f"{target}_shift_{i}" for i in periods_online]
+shift_features = shift_features_online + shift_features_batch
+
 numerical_features = (
     ["sell_price"]
     + aggregated_features
     + calendar_features
-    + lag_features
     + pct_change_features
+    + rolling_features
+    + shift_features
 )
 
 features = binary_features + categorical_features + numerical_features
