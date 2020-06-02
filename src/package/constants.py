b8f73e7576b922aa7ce2ea023700983292a6cd58
Y-oHr-N <kon.y.ohr.n@gmail.com>
2020-06-02 09:35:50 +0900

    Apply black

--- src/package/constants.py
+++ src/package/constants.py
@@ -178,7 +178,8 @@ pct_change_features = [
 ]
 
 scaled_features = [
-    f"scaled_{raw_numerical_feature}" for raw_numerical_feature in raw_numerical_features
+    f"scaled_{raw_numerical_feature}"
+    for raw_numerical_feature in raw_numerical_features
 ]
 
 shift_features_batch = [f"{target}_shift_{i}" for i in periods_batch]
