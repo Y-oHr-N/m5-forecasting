110d936a79db7f54379ebf979cea5cf70bc4b319
Y-oHr-N <kon.y.ohr.n@gmail.com>
2020-06-02 23:57:10 +0900

    Add detrend

--- src/package/preprocessing.py
+++ src/package/preprocessing.py
@@ -1,7 +1,17 @@
+import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 from sklearn.utils import check_random_state
 
-__all__ = ["add_gaussian_noise", "label_encode"]
+from .constants import *
+
+__all__ = [
+    # General functions
+    "add_gaussian_noise",
+    "label_encode",
+    # Specidifc functions
+    "detrend",
+]
 
 
 def add_gaussian_noise(df, cols, sigma=0.01, random_state=None):
@@ -16,3 +26,35 @@ def label_encode(df, cols):
     for col in cols:
         codes, _ = pd.factorize(df[col], sort=True)
         df[col] = codes
+
+
+def detrend(df):
+    model = LinearRegression()
+
+    X = np.arange(train_days + 2 * test_days)
+    X = X.reshape(-1, 1)
+
+    Y = df.pivot_table(columns=level_ids[11], dropna=False, index="date", values=target)
+
+    is_selled = df.pivot_table(
+        columns=level_ids[11], dropna=False, index="date", values="sell_price"
+    )
+    is_selled = is_selled.notnull()
+
+    Y_mean = Y[is_selled].mean()
+
+    Y.where(is_selled, Y_mean, axis=1, inplace=True)
+
+    model.fit(X[:train_days], Y.iloc[:train_days])
+
+    tmp = model.predict(X)
+    tmp = pd.DataFrame(tmp, columns=Y.columns, index=Y.index)
+    tmp = tmp.unstack()
+    on = ["item_id", "store_id", "date"]
+
+    tmp.rename("linear_trend", inplace=True)
+
+    tmp = df[on].merge(tmp, copy=False, on=on)
+
+    df["linear_trend"] = tmp["linear_trend"]
+    df[f"detrended_{target}"] = df[target] - tmp["linear_trend"]
