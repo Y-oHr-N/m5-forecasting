c1145904076ddc632eb59438a2e972cc2dbb9708
Y-oHr-N <kon.y.ohr.n@gmail.com>
2020-06-16 12:54:04 +0900

    Update load_iterim (accuracy: 0.48554, uncertainty: 0.15826)

--- src/package/datasets.py
+++ src/package/datasets.py
@@ -15,18 +15,26 @@ def load_interim():
 
     interim_dir_path.mkdir(parents=True, exist_ok=True)
 
+    calendar = pd.read_csv(calendar_path, dtype=dtype, parse_dates=parse_dates)
     sales_train_validation = pd.read_csv(sales_train_validation_path, dtype=dtype)
+    sell_prices = pd.read_csv(sell_prices_path, dtype=dtype)
 
     for i in range(train_days + 1, train_days + 1 + 2 * test_days):
         sales_train_validation[f"d_{i}"] = np.nan
-
-    reduce_memory_usage(sales_train_validation, allow_float16=False)
 
     interim = sales_train_validation.melt(
         id_vars=["id", "item_id", "store_id", "dept_id", "cat_id", "state_id"],
         var_name="d",
         value_name=target,
     )
+    interim = interim.merge(calendar, copy=False, how="left", on="d")
+    interim = interim.merge(
+        sell_prices, copy=False, how="left", on=["store_id", "item_id", "wm_yr_wk"]
+    )
+
+    interim.reset_index(drop=True, inplace=True)
+
+    reduce_memory_usage(interim, allow_float16=False)
 
     interim.to_parquet(interim_path)
 
